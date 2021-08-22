2af2a01a784a689b9716eea076a7f43a7633b9bd
GuYun-D <1946306647@qq.com>
2021-08-22 11:49:41 +0800

    最小量更新

---
+++ snabbdom/src/02 - test-my-FunctionH.js
@@ -0,0 +1,9 @@
+import h from './mySnabbdom/h'
+
+// 测试h函数
+var vnode1 = h("div", {}, [
+  h('p', {}, '哈哈'),
+  h('p', {}, '呵呵'),
+  h('p', {}, h('a', {}, 'text'))
+])
+console.log(vnode1);
--- snabbdom/src/index.js
+++ snabbdom/src/index.js
@@ -1,9 +1,48 @@
-import h from './mySnabbdom/h'
+import {
+  init,
+  classModule,
+  propsModule,
+  styleModule,
+  eventListenersModule,
+  h,
+} from "snabbdom";
 
-// 测试
-var vnode1 = h("div", {}, [
-  h('p', {}, '哈哈'),
-  h('p', {}, '呵呵'),
-  h('p', {}, h('a', {}, 'text'))
+/**
+ * 体验diff算法
+ */
+const container = document.getElementById('container')
+const btn = document.getElementById('btn')
+
+
+// 创建patch函数
+const patch = init([classModule, propsModule, styleModule, eventListenersModule])
+
+var vnode1 = h('ul', {}, [
+  h('li', {key: 'A'}, 'A'),
+  h('li', {key: 'B'}, 'B'),
+  h('li', {key: 'C'}, 'C'),
+  h('li', {key: 'D'}, 'D'),
+  h('li', {key: 'E'}, 'E')
 ])
-console.log(vnode1);
+
+// 上树
+patch(container, vnode1)
+
+var vnode2 = h('ul', {}, [
+  h('li', {key: "G"}, '头部添加'),
+  h('li', {key: "A"}, 'A'),
+  h('li', {key: "B"}, 'B'),
+  h('li', {key: "C"}, 'C'),
+  h('li', {key: "D"}, 'D'),
+  h('li', {key: "E"}, 'E'),
+  h('li', {key: "F"}, 'F'),
+])
+
+btn.addEventListener('click', function () {
+  /**
+   * 验证：点击按钮前，通过开发者工具，修改vnode1的文本属性，再点击按钮，发现，vnode1部分没有重新渲染，
+   * 而是改动了该改动的地方
+   */
+  patch(vnode1, vnode2)
+})
+
