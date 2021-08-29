db45fc5955c8e91084ee383fbfa3873d956e2b90
GuYun-D <1946306647@qq.com>
2021-08-29 16:13:23 +0800

    修复在老节点头部添加时的bug

--- snabbdom/src/index.js
+++ snabbdom/src/index.js
@@ -15,15 +15,28 @@ const vnode1 = h('ul', {},
 )
 
 const vnode2 = h('ul', {}, [
+  h('li', { key: "M" }, "M"),
   h('li', { key: 'C' }, 'C'),
   h('li', { key: 'Q' }, 'Q'),
-  h('li', { key: 'B' }, 'B')
+  h('li', { key: 'B' }, h('a', { props: { href: "http://www.baidu.com" } }, "百度"))
 ])
+
+// beforebug测试
+const vnode3 = h('ul', {},
+  [
+    h('li', { key: 'Q' }, 'Q'),
+    h('li', { key: 'A' }, 'A'),
+    h('li', { key: 'B' }, 'B'),
+    h('li', { key: 'C' }, 'C'),
+    h('li', { key: 'D' }, 'D'),
+    h('li', { key: 'E' }, 'E')
+  ]
+)
 
 // var vnode1 = h('mark', {}, '哈哈哈哈哈哈哈哈哈')
 
 patch(container, vnode1)
 
 btn.addEventListener('click', function () {
-  patch(vnode1, vnode2)
+  patch(vnode1, vnode3)
 })
