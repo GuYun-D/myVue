23ad92d318f97aff3caa54a77a261d8f76a64439
GuYun-D <1946306647@qq.com>
2021-08-30 09:44:35 +0800

    defineProperty方法使用

--- data_reactive_vue2/src/index.js
+++ data_reactive_vue2/src/index.js
@@ -1,1 +1,27 @@
-alert("打开了")
+var obj = {}
+
+/**
+ * 参数一：定义哪一个对象
+ * 参数二：要定义的属性
+ * 参数三：该属性的操作符
+ */
+// 创建一个中转变量
+var temp;
+
+Object.defineProperty(obj, 'a', {
+  get(){
+    return temp
+  },
+
+  set(newVal){
+    temp = newVal
+    return temp
+  }
+})
+
+Object.defineProperty(obj, 'b', {
+  value: 5
+})
+
+obj.a = 1
+console.log(obj.a);
